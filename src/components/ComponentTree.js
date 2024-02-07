f067abf46f12969f3934332f568c03b747fc1b75
Sufyan-007 <139311956+Sufyan-007@users.noreply.github.com>
2024-02-07 15:36:56 +0530

    Removed RootComponent and using navigate to view components

--- src/components/ComponentTree.js
+++ src/components/ComponentTree.js
@@ -1,21 +1,19 @@
 import { useSelector } from 'react-redux'
 import { useRef, useState, useContext } from 'react'
 import { ServiceContext } from "../store/Context";
-import { Modal, Button, Form} from 'react-bootstrap'
+import { Modal, Button, Form } from 'react-bootstrap'
 import rightArrow from "../assets/icons/arrow_right_icon.svg"
 import downArrow from "../assets/icons/arrow_down_icon.svg"
-import RootComponent from './RootComponent'
-import { router } from '../App'
-
-export default function ComponentTree({  ...props }) {
+import { router } from '../App';
+export default function ComponentTree({ ...props }) {
     const config = useSelector((state) => state.config)
     const { configService } = useContext(ServiceContext)
-    
+
 
 
     const [showDropdown, setShowDropdown] = useState(false)
-    const [ showModal, setShowModal] = useState(false)
-    const newCompRef= useRef()
+    const [showModal, setShowModal] = useState(false)
+    const newCompRef = useRef()
 
 
 
@@ -24,24 +22,25 @@ export default function ComponentTree({  ...props }) {
     }
 
 
-    function closeModal(added){
-        if(added){
-            const name=newCompRef.current[0].value
+    function closeModal(added) {
+        if (added) {
+            const name = newCompRef.current[0].value
             const route = newCompRef.current[1].value
-            if(name){
-                configService.addComponent(name,route)
+            if (name) {
+                configService.addComponent(name, route)
             }
         }
         setShowModal(false)
     }
 
-    
+    function openComponent(name) {
+        router.navigate("comp/"+name)
+    }
 
-    
 
     return (
         <div {...props}>
-            <Modal show={showModal} onHide={()=>closeModal(false)}>
+            <Modal show={showModal} onHide={() => closeModal(false)}>
                 <Modal.Header>
                     Add Component
                 </Modal.Header>
@@ -51,7 +50,7 @@ export default function ComponentTree({  ...props }) {
                             <Form.Label>
                                 Component Name
                             </Form.Label>
-                            <Form.Control  />
+                            <Form.Control />
                         </Form.Group>
                         <Form.Group name="route">
                             <Form.Label>
@@ -63,13 +62,13 @@ export default function ComponentTree({  ...props }) {
                 </Modal.Body>
 
                 <Modal.Footer>
-                        <Button variant="secondary" onClick={() => { closeModal(false); }} >
-                            Cancel
-                        </Button>
-                        <Button variant="primary" onClick={() => { closeModal(true); }}>
-                            Update
-                        </Button>
-                    </Modal.Footer>
+                    <Button variant="secondary" onClick={() => { closeModal(false); }} >
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={() => { closeModal(true); }}>
+                        Update
+                    </Button>
+                </Modal.Footer>
             </Modal>
             <div className="container-fluid my-1">
                 <div className="row">
@@ -85,19 +84,27 @@ export default function ComponentTree({  ...props }) {
                     </div>
                 </div>
                 {showDropdown ?
-                    Object.entries(config).map(([k, v]) => <RootComponent key={k} name={k} component={v}  className=" row m-1 " />)
+                    Object.entries(config).map(([k, v]) =>
+                        <div key={k} name={k} component={v} className=" row m-1 " >
+                            <div className="container-fluid border border-dark-subtle border-2">
+                                <div className=" d-flex justify-content-between my-2 fw-bold">
+                                    <button className="btn text-white" onClick={()=>openComponent(k)} >{k}</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
 
                     : null
                 }
-                {showDropdown?
+                {showDropdown ?
                     <div className="row my-2">
                         <div className='d-flex justify-content-around'>
-                            <button className='   btn btn-secondary ' onClick={()=>setShowModal(true)}>
+                            <button className='   btn btn-secondary ' onClick={() => setShowModal(true)}>
                                 Add Component
                             </button>
                         </div>
                     </div>
-                :null}
+                    : null}
             </div>
         </div>
     )
