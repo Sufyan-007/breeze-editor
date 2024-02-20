3a99e7d5ce8cfa109a4eda027c2eda25744224be
Sufyan-007 <139311956+Sufyan-007@users.noreply.github.com>
2024-02-20 11:51:03 +0530

    Redux Config page

---
+++ src/components/Reducers.js
@@ -0,0 +1,13 @@
+export default function Reducers({...props}){
+    return (
+        <div {...props}>
+            <div className="container-fluid my-1">
+                <div className="row">
+                    <div className="d-flex  fw-bold fs-5">
+                        Reducers
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/ReduxStore.js
@@ -0,0 +1,13 @@
+export default function ReduxStore({...props}){
+    return (
+        <div {...props}>
+            <div className="container-fluid my-1">
+                <div className="row">
+                    <div className="d-flex  fw-bold fs-5">
+                        Redux Stores
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
