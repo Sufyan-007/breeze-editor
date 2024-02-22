6aabc8bc72eed2a286959d5d10874078430e28dc
Sufyan-007 <139311956+Sufyan-007@users.noreply.github.com>
2024-02-22 14:43:56 +0530

    Added API for store+redux config

--- breeze/apps/editor_api/urls.py
+++ breeze/apps/editor_api/urls.py
@@ -4,13 +4,19 @@ from .views import ComponentWriter
 from .views import RoutingReader
 from .views import NewComponentWriter
 from .views import RoutingWriter
-from .views import ReduxWriter
+from .views import ProjectReader
+from .views import ReducerConfig
+from .views import StoreConfig
 urlpatterns = [
         path('read-config/<str:param>/',ConfigReader.as_view()),
         path('write-config/<str:param>/',ComponentWriter.as_view()),
         path('read-router-config/<str:param>/',RoutingReader.as_view()),
         path('add-component/<str:param>/',NewComponentWriter.as_view()),
         path('add-route/<str:param>/',RoutingWriter.as_view()),
-        path('write-redux/<str:param>/',ReduxWriter.as_view())
+        path('read-reducers/<str:param>/',ReducerConfig.as_view()),
+        path('write-reducers/<str:param>/',ReducerConfig.as_view()),
+        path('read-redux-store/<str:param>/',StoreConfig.as_view()),
+        path('write-redux-store/<str:param>/',StoreConfig.as_view()),
+        path('all-projects',ProjectReader.as_view()),
 ]
 
--- breeze/apps/editor_api/views.py
+++ breeze/apps/editor_api/views.py
@@ -2,6 +2,7 @@
 from django.http import JsonResponse
 import json
 from .core.app_editor import AppEditor
+from .core.generate_project import GenerateProject
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
@@ -58,14 +59,45 @@ class RoutingWriter(APIView):
         except:
             return JsonResponse({}, status=500)
 
+
 @method_decorator(csrf_exempt,name='dispatch')
-class ReduxWriter(APIView):
-    def post(self,request,param):
-        # data= json.loads(request.body.decode("utf-8"))
+class ReducerConfig(APIView):
+    def get(self,request,param):
         try:
             app_editor= AppEditor(param)
-            app_editor.write_reducers()
-            app_editor.write_redux_store()
-            return JsonResponse({},status=200)
+            return JsonResponse(app_editor.get_reducer_config(),status=200)
+        except:
+            return JsonResponse({},status=404)
+    def post(self,request,param):
+        data= json.loads(request.body.decode("utf-8"))
+        try:
+            app_editor= AppEditor(param)
+            res=app_editor.write_reducers_config(data)
+            return JsonResponse(res,status=200)
+        except:
+            return JsonResponse({},status=404)
+
+@method_decorator(csrf_exempt,name='dispatch')
+class StoreConfig(APIView):
+    def get(self,request,param):
+        try:
+            app_editor= AppEditor(param)
+            return JsonResponse(app_editor.get_redux_store_config(),status=200)
+        except:
+            return JsonResponse({},status=404)
+
+    def post(self,request,param):
+        data= json.loads(request.body.decode("utf-8"))
+        try:
+            app_editor= AppEditor(param)
+            store_config=app_editor.write_redux_config(data)
+            # app_editor.write_redux_store()
+            return JsonResponse(store_config,status=200)
         except:
             return JsonResponse({}, status=500)
+
+@method_decorator(csrf_exempt,name='dispatch')
+class ProjectReader(APIView):
+    def get(self,param):
+        projects = GenerateProject.get_projects()
+        return JsonResponse(projects,status=200)
