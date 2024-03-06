97cbe5b44b66ac4dc5a5c5ca6a0b0b99a04db8cc
sgazdhar <sgazdhar@argusoft.com>
2024-03-06 14:56:50 +0530

    Added button for delete

--- editor_ui/src/services/ProjectService.js
+++ editor_ui/src/services/ProjectService.js
@@ -11,3 +11,13 @@ export async function reGenerateProject(projectName){
     )).json()
     return respnse
 }
+
+export async function deleteProject(projectName){
+    const response = await fetch("http://localhost:8000/editor/delete-project/"+projectName+"/",
+        {method:"DELETE",headers: { 'Content-Type': 'application/json' }}
+    )
+    const status = response.status
+    const body = await response.json()
+    await new Promise(r => setTimeout(r, 2000));
+    return {status,body}
+}
