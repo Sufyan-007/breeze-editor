63adf577b16194daa48abe02a168702a588ce360
sgazdhar <sgazdhar@argusoft.com>
2024-03-06 14:56:28 +0530

    added API for deleting

--- breeze/apps/editor_api/core/generate_project.py
+++ breeze/apps/editor_api/core/generate_project.py
@@ -1,4 +1,4 @@
-import subprocess,os
+import subprocess,os,shutil
 
 from common.utils.config_reader import read_config_file
 from .app_generator import AppGenerator 
@@ -27,3 +27,16 @@ class GenerateProject:
             app_config['project_name'] = project_name
             projects[project_name]=app_config
         return projects
+    
+    @staticmethod
+    def delete_project(project_name):
+        app_config_dir = f"{CONFIG_PATH}/{project_name}"
+        try:
+            app_config = read_config_file(app_config_dir, CONFIG_FILES_PATH['APP_CONFIG'])
+            generated_project_path = app_config['path']
+        except:
+            raise FileNotFoundError("Could not find project '{project_name}")
+        print(app_config_dir)
+        print(generated_project_path)
+        shutil.rmtree(generated_project_path)
+        shutil.rmtree(app_config_dir)
