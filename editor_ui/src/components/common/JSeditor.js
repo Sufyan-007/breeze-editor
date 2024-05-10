f947af154b2920fbe830a92da12ea09105db1568
hsutaria <hsutaria@argusoft.com>
2024-05-10 12:24:33 +0530

    common monaco editor component

---
+++ editor_ui/src/components/common/JSeditor.js
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from 'react';
+import * as monaco from 'monaco-editor';
+
+function JSeditor({ defaultValue = "", height = "500px", width = "100%", language = "javascript", theme = "vs-dark", onChange, id="monaco-editor" }) {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    const editorDiv = document.getElementById(id);
+    if (editorDiv) {
+      const editor = monaco.editor.create(editorDiv, {
+        value: defaultValue,
+        language,
+        theme,
+      });
+      editorRef.current = editor;
+
+      editor.onDidChangeModelContent(() => {
+        if (onChange) {
+          onChange(editor.getValue());
+        }
+      });
+
+      return () => editor.dispose();
+    }
+  }, [defaultValue, language, theme, id, onChange]);
+
+  // const getValue = useCallback(() => {
+  //   return editorRef.current ? editorRef.current.getValue() : "";
+  // }, []);
+
+  return (
+    <div>
+      <div id={id} style={{ height, width }}></div>
+    </div>
+  );
+}
+
+export default JSeditor;
