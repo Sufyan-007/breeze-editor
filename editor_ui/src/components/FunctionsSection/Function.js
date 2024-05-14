40afbb3edf2a58290ff984c8c13b6192dc0b0b3e
sgazdhar <sgazdhar@argusoft.com>
2024-05-14 15:39:10 +0530

    Updated Monaco Editor

--- editor_ui/src/components/FunctionsSection/Function.js
+++ editor_ui/src/components/FunctionsSection/Function.js
@@ -5,13 +5,9 @@ import { useEffect, useState } from "react"
 import MonacoEditor from "../common/MonacoEditor"
 
 export default function Function({ func }) {
-    console.log(func)
+
     const [showDetails, setShowDetails] = useState(false)
-    const elem =document.getElementById("lets-try-this)")
-    console.log(elem)
-    useEffect(()=>{
-        console.log("Elem has been changed")
-    },[elem])
+    
     return (
         <div className={" container-fluid  border p-2 " + (showDetails ? "bg-dark" : "")}>
             <div className="d-flex px-1 " style={{ cursor: 'pointer' }} onClick={() => setShowDetails(state => !state)} >
@@ -30,7 +26,7 @@ export default function Function({ func }) {
                     {func?.description ? func.description : " No description"}
                 </div>
             </div>
-            {(showDetails||true) &&
+            {(showDetails) &&
                 <>
                     <div className="row mx-2 " >
                         Parameters
@@ -39,11 +35,11 @@ export default function Function({ func }) {
                         Function Body
                     </div>
                     <div id="lets-try-this" className="row m-3">
-                        {/* <MonacoEditor
+                        <MonacoEditor
                             defaultValue=""
                             id={func['$id']}
-                            width="90%"
-                        /> */}
+                            width="50%"
+                        />
                     </div>
                 </>
 
--- editor_ui/src/components/common/MonacoEditor.js
+++ editor_ui/src/components/common/MonacoEditor.js
@@ -1,28 +1,43 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import * as monaco from 'monaco-editor';
 
-function MonacoEditor({ defaultValue = "", height = "500px", width = "100%", language = "javascript", theme = "vs-dark", onChange, id="monaco-editor" }) {
-  const editorRef = useRef(null);
+function MonacoEditor({ defaultValue = "", height = "500px", width = "100%", language = "javascript", theme = "vs-dark", onChange, id = "monaco-editor" }) {
+  const [editor, setEditor] = useState(null)
+  const editorRef = useRef()
+  // const [editorDiv,setEditorDiv] = useState(document.getElementById(id));
+  
+  // useEffect(()=>{
+  //   setEditorDiv(document.getElementById(id))
+  // })
 
   useEffect(() => {
+    console.log("setChange")
+    if (editor) {
+      editor.onDidChangeModelContent(() => {
+        if (`onChange`) {
+          onChange(editor.getValue());
+        }
+      });
+    }
+  }, [editor, onChange])
+
+
+  useEffect(() => {
+    console.log("INit editor")
     const editorDiv = document.getElementById(id);
     if (editorDiv) {
+
+      console.log("Actual init")
       const editor = monaco.editor.create(editorDiv, {
         value: defaultValue,
         language,
         theme,
       });
       editorRef.current = editor;
-
-      editor.onDidChangeModelContent(() => {
-        if (onChange) {
-          onChange(editor.getValue());
-        }
-      });
-
+      setEditor(editor);
       return () => editor.dispose();
     }
-  }, [defaultValue, language, theme, id, onChange]);
+  }, [defaultValue,  language, theme, id]);
 
   // const getValue = useCallback(() => {
   //   return editorRef.current ? editorRef.current.getValue() : "";
