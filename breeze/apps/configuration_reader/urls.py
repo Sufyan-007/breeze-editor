1188ff9004729823555811f635afe454b31fcf1d
rpatel <rpatel@argusoft.in>
2024-05-16 12:57:42 +0530

    API added to read other config

--- breeze/apps/configuration_reader/urls.py
+++ breeze/apps/configuration_reader/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GetComponents, GetComponentConfig 
+from .views import GetComponents, GetComponentConfig, ReadFromPath
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
         path('get-components/', csrf_exempt(GetComponents.as_view())),
-        path('get-component-config/', csrf_exempt(GetComponentConfig.as_view()))
+        path('get-component-config/', csrf_exempt(GetComponentConfig.as_view())),
+        path('get-from-path/<str:filepath>/', csrf_exempt(ReadFromPath.as_view()))
 ]
 
--- breeze/apps/configuration_reader/views.py
+++ breeze/apps/configuration_reader/views.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.http import JsonResponse
 import json
 from .core.app_config_reader import AppConfigReader
+from common.utils.app_consts import THIRD_PARTY_CONFIG_PATH
+from common.utils.config_reader import read_file_json
 
 class GetComponents(View):
     
@@ -22,3 +24,14 @@ class GetComponentConfig(View):
         response = app_config_reader.get_component_config(data)
 
         return JsonResponse(response, status = 200)
+    
+class ReadFromPath(View):
+
+    def get(self, request, filepath):
+        # filepath = filepath.replace("@@@", "/")
+        full_path = f"{THIRD_PARTY_CONFIG_PATH}/react-bootstrap/others/{filepath}"
+
+        response = read_file_json(full_path)
+
+        return JsonResponse(response, status = 200)
+
