85875205e0bc98ca57f498792577ec3f1f41512f
sgazdhar <sgazdhar@argusoft.com>
2024-05-27 16:42:36 +0530

    Added editing parameter details

--- editor_ui/src/components/FunctionsSection/Function.js
+++ editor_ui/src/components/FunctionsSection/Function.js
@@ -1,21 +1,30 @@
 
-import rightArrow from "../../assets/icons/arrow_right_icon.svg"
-import downArrow from "../../assets/icons/arrow_down_icon.svg"
 import { useEffect, useState } from "react"
 import MonacoEditor from "../common/MonacoEditor"
 import { updateFunction } from "../../services/FunctionConfigService"
 import { useParams } from "react-router"
 import CommonConfigSidebar from "../common/CommonConfigSidebar"
+import FunctionParams from "./FunctionParams"
 
 export default function Function({ func, updateFunctions }) {
     const [functionConfig, setFunctionConfig] = useState(func)
     const [showDetails, setShowDetails] = useState(false)
     const { projectName, componentName } = useParams()
+    const [newParam, setNewParam] = useState(null)
 
     useEffect(() => {
         setFunctionConfig(func)
     }, [func])
 
+
+    function updateParams(param,index){
+        
+        setFunctionConfig(functionConfig=>{
+            functionConfig.parameters.list[index] =param
+
+            return {...functionConfig}
+        })
+    }
 
     function updateFunctionConfig() {
         console.log('update function config')
@@ -25,11 +34,34 @@ export default function Function({ func, updateFunctions }) {
         })
     }
 
-    return (
+    function updateNewParam(param) {
+        param = param.target.value.replace(/\s+/g, "")
+        setNewParam(param)
+    }
 
+    function addParam() {
+        if (newParam === null) {
+            setNewParam("")
+        }
+        else {
+            if (newParam !== "") {
+                setFunctionConfig((functionConfig) => {
+                    functionConfig.parameters.list.push({
+                        name: newParam
+                    })
+                    return functionConfig
+                })
+                setNewParam(null)
+            } else {
+                alert("Param cannot be null")
+            }
+        }
+    }
+
+    return (
         <div className={" container-fluid  border p-2 " + (showDetails ? "bg-dark" : "")}>
             <div className="d-flex px-1 " style={{ cursor: 'pointer' }} onClick={() => setShowDetails(state => !state)} >
-               
+
                 <div className=" fs-5 ">
                     {func.name}
                 </div>
@@ -42,32 +74,56 @@ export default function Function({ func, updateFunctions }) {
                 {showDetails &&
                     <>
                         <div className="row mx-2" >
-                            <div className="col-2 ">
+                            <div className="col-2 fs-5">
                                 Description
                             </div>
                             <div className="col">
                                 <textarea className=" w-75" value={functionConfig.description} onChange={(event) => setFunctionConfig(state => { return { ...state, description: event.target.value } })} placeholder="Description" name="" id=""></textarea>
                             </div>
                         </div>
+
+                        <div className="row mx-2 my-2" >
+                            <div className="col ">
+                                <div className="fs-5">Function Type</div>
+                                <div className=" ms-3">
+                                    <input type="checkbox" checked={functionConfig.isAnonymous} onChange={(event) => setFunctionConfig(state => { return { ...state, isAnonymous: event.target.checked } })} className="my-2 me-2" />
+                                    Anonymous
+                                </div>
+                                <div className=" ms-3">
+                                    <input type="checkbox" checked={functionConfig.isAsync} onChange={(event) => setFunctionConfig(state => { return { ...state, isAsync: event.target.checked } })} className="my-2 me-2" />
+                                    Async
+                                </div>
+                            </div>
+                        </div>
+
+
                         <div className="row mx-2 my-1 " >
-                            <div className="col">Parameters</div>
-                        </div>
-
-                        <div className="row mx-2 " >
                             <div className="col">
-                                <input type="checkbox" checked={functionConfig.isAnonymous} onChange={(event) => setFunctionConfig(state => { return { ...state, isAnonymous: event.target.checked } })} className="my-2 me-2" />
-                                Anonymous
+                                <div className=" d-flex">
+
+                                    <div className=" fs-5">Parameters</div>
+                                </div>
+
+                                {functionConfig.parameters.list.map((param, index) =>
+                                    <div className="row">
+                                        <FunctionParams param={param} setParam={(param) => updateParams(param, index) } />
+                                    </div>
+                                )
+                                }
+                                <div className="row">
+                                    <div className=" m-1">
+                                        {newParam !== null &&
+                                            <input type="text" value={newParam} onChange={updateNewParam} className=" p-0 mx-2 " />
+                                        }
+                                        <button className="p-1 btn btn-sm btn-secondary" onClick={addParam}>
+                                            Add Param
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
-
-                        <div className="row mx-2 " >
-                            <div className="col">
-                                <input type="checkbox" checked={functionConfig.isAsync} onChange={(event) => setFunctionConfig(state => { return { ...state, isAsync: event.target.checked } })} className="my-2 me-2" />
-                                Async
-                            </div>
-                        </div>
-                        <div className="row mx-2 mt-3">
+                        <div className="row mx-2 mt-3 fs-5">
                             <div className="col">Function Body</div>
                         </div>
                         <div id={"function-body-" + func["$id"]} className="row mx-2">
