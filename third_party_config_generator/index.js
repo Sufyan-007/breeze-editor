68fd3e8139d11bc75c894c9bf207be3b17153de6
rpatel <rpatel@argusoft.in>
2024-05-27 15:14:05 +0530

    Added package installation logic tp config generator node app

--- third_party_config_generator/index.js
+++ third_party_config_generator/index.js
@@ -1,4 +1,5 @@
 const config_generator = require('./src/config_generator')
+const packageInstaller = require('./src/package_installer')
 
 
 // const lib = 'react-bootstrap'
@@ -12,12 +13,18 @@ function generate_config(libraryName, storePath){
 
 // console.log(process.argv);
 if (process.argv.length >= 3) {
-    var functionName = process.argv[2];
+    const functionName = process.argv[2];
     
-  
     // Call the specified function with additional arguments if provided
     if (functionName === "generate_config" && process.argv.length >= 4) {
-      generate_config(process.argv[3], process.argv[4]);
+      const libName = process.argv[3]
+      const libVersion = process.argv[4]
+      const storePath = process.argv[5]
+      // Try to install library
+      packageInstaller.installPackage(libName, libVersion);
+
+      // Generate configs for the library
+      // generate_config(libName, storePath);
     }
   }
 
