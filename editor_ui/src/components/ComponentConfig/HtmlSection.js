7a1852c12dde2deebbd0ed0cea8c7695db568916
sgazdhar <sgazdhar@argusoft.com>
2024-06-12 16:00:41 +0530

    Fixed merge issues

--- editor_ui/src/components/ComponentConfig/HtmlSection.js
+++ editor_ui/src/components/ComponentConfig/HtmlSection.js
@@ -22,6 +22,7 @@ export default function HtmlSection() {
         return new MessageListenerService(projectName, componentName)
     }, [projectName, componentName])
 
+    const [testProps, setTestProps] = useState({ prop1: "xyz" })
 
     const setIframeSource = () => {
         const newValue = srcInput.current.value;
@@ -29,15 +30,41 @@ export default function HtmlSection() {
     };
 
     useEffect(() => {
+        const handler = (message) => {
+            if (message.data.source === "APP") {
+                if (message.data.type === "request") {
+                    console.log("got request", message.data)
+                    if (message.data.request.type === "props") {
+                        const iframe = document.getElementById("iFrame")
+                        iframe.contentWindow.postMessage({ type: "resource", resource: { type: "props", props: testProps } }, "*")
 
-        window.addEventListener("message", (message) => {
+                    }
+                }
+            }
+        }
+        window.addEventListener("message", handler)
+        return () => {
+            window.removeEventListener("message", handler)
+        }
+    }, [testProps])
+
+    useEffect(() => {
+        const handler = (message) => {
             if (message.data.source === "APP") {
                 console.log(message.data)
                 if (message.data.type === "elementDrop") {
                     messageListener.onElementDrop(message.data)
                 }
+                if (message.data.type === "request") {
+                    const request = message.data.request
+                    if (request.type === "component") {
+                        const iframe = document.getElementById("iFrame")
+                        iframe.contentWindow.postMessage({ type: "resource", resource: { type: "component", component: componentConfig } }, "*")
+                    }
+                }
             }
-        })
+        }
+        window.addEventListener("message", handler)
         setTimeout(async () => {
             const iframe = document.getElementById("iFrame")
 
@@ -45,52 +72,62 @@ export default function HtmlSection() {
                 iframe.contentWindow.postMessage({ func: '()=>{console.log(" Hello World") }' }, "*")
             }
         }, 500)
-    }, [messageListener])
+        return () => {
+            window.removeEventListener("message", handler)
+        }
+    }, [messageListener, componentConfig])
 
 
     return (
-        <DragContext.Provider value={{messageListener}} >
+        <DragContext.Provider value={{ messageListener }} >
             <div className="row flex-grow-1" style={{ position: "relative" }}>
 
-            <div className="text-white col-3 h-100" style={{ width: "18rem", backgroundColor: "#303033" }}>
-                <div className="row p-2">
-                    <div className="border border-dark px-0">
-                        <div
-                            className="py-2 btn rounded-0 text-white col-4 border-right border-dark"
-                            style={selected === 0 ? { backgroundColor: "rgb(33, 37, 41) " } : { backgroundColor: "#303033" }}
-                            onClick={() => setSelected(0)}
-                        >
-                            Html
-                        </div>
-                        <div
-                            className="py-2 btn rounded-0 text-white col-4 border-dark"
-                            style={selected === 1 ? { backgroundColor: "rgb(33, 37, 41) " } : { backgroundColor: "#303033" }}
-                            onClick={() => setSelected(1)}
-                        >
-                            Add El
-                        </div>
-                        <div
-                            className="py-2 btn rounded-0 text-white col-4 border-left border-dark"
-                            style={selected === 2 ? { backgroundColor: "rgb(33, 37, 41) " } : { backgroundColor: "#303033" }}
-                            onClick={() => setSelected(2)}
-                        >
-                            Actions
+                <div className="text-white  d-flex flex-column col-3 h-100" style={{ width: "18rem", backgroundColor: "#303033" }}>
+                    <div className="row p-2">
+                        <div className="border border-dark px-0">
+                            <div
+                                className="py-2 btn rounded-0 text-white col-6 border-right border-dark"
+                                style={selected === 0 ? { backgroundColor: "rgb(33, 37, 41) " } : { backgroundColor: "#303033" }}
+                                onClick={() => setSelected(0)}
+                            >
+                                Html
+                            </div>
+                            <div
+                                className="py-2 btn rounded-0 text-white col-6 border-left border-dark"
+                                style={selected === 1 ? { backgroundColor: "rgb(33, 37, 41) " } : { backgroundColor: "#303033" }}
+                                onClick={() => setSelected(1)}
+                            >
+                                Actions
+                            </div>
                         </div>
                     </div>
+                    {selected === 0 ?
+                        <>
+                            <div className=" row flex-grow-1">
+                                <div className=" d-flex h-50">
+                                    <div className="col">
+                                        <HtmlTree htmlId={componentName} config={componentConfig} className="row my-1" />
+                                    </div>
+                                </div>
+                                <div className=" d-flex h-50">
+                                    <div className="col">
+                                        <div className="row border-top border-3 border-black">
+                                            Add Elements
+                                        </div>
+                                        <AddElements />
+                                    </div>
+                                </div>
+                            </div>
+
+                        </>
+                        :
+                        <div>
+                            <ActionsConfig />
+                        </div>
+                    }
                 </div>
-                {selected === 0 ?
-                    <HtmlTree htmlId={componentName} config={componentConfig} className="row my-1" />
-                    :
-                    selected===1?
-                        <div>Add element</div>
-                    :
-                    <div>
-                        <ActionsConfig />
-                    </div>  
-                }
-            </div>
-            <div className="col overflow-hidden p-0">
-                <div className=" bg-dark-subtle align-items-center d-flex justify-content-start" style={{ 'height': "2.4rem" }}>
+                <div className="col overflow-hidden p-0">
+                    <div className=" bg-dark-subtle align-items-center d-flex justify-content-start" style={{ 'height': "2.4rem" }}>
 
                         <div className="me-3">
                             <input
