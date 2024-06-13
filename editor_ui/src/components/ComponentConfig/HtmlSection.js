150eef0548e94a4936f5ed5efea316a1411e25e4
sgazdhar <sgazdhar@argusoft.com>
2024-06-13 10:11:22 +0530

    Added env variable for generated project host

--- editor_ui/src/components/ComponentConfig/HtmlSection.js
+++ editor_ui/src/components/ComponentConfig/HtmlSection.js
@@ -13,7 +13,7 @@ export const DragContext = createContext({
 
 export default function HtmlSection() {
     const { componentConfig } = useContext(ComponentContext)
-    const [iframeSrc, setIframeSrc] = useState(`${process.env.REACT_APP_BREEZE_BACKEND_HOST}` + componentConfig.port)
+    const [iframeSrc, setIframeSrc] = useState(`${process.env.REACT_APP_GENERATED_PROJECT_DOMAIN}:` + componentConfig.port)
     const srcInput = useRef()
     const [selected, setSelected] = useState(0)
     const iFrameRef = useRef();
