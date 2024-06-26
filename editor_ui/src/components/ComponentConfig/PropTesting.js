6404a66178e560e98e8ee7f0dd713f50e06ffaab
AnshikX <athakur@argusoft.com>
2024-06-26 11:33:50 +0530

    no props condition

--- editor_ui/src/components/ComponentConfig/PropTesting.js
+++ editor_ui/src/components/ComponentConfig/PropTesting.js
@@ -65,25 +65,35 @@ export default function PropTesting() {
         onClose={() => setCanvasOpen(false)}
         title="Prop configuration"
       >
-        {componentConfig.propsVars.map((prop) => (
-          <div key={prop.name} className="mb-4 row">
-            <label className="col-sm-3 col-form-label text-white">
-              {prop.name}:
-            </label>
-            <div className="col-sm-9 mt-2">
-              <MonacoEditor
-                value={formInputs[prop.name]?.value || ""}
-                onChange={(value) => handleInputChange(prop.name, value)}
-                height="50px"
-                id={prop.id}
-                width="80%"
-                language="javascript"
-              />
-            </div>
+        {componentConfig.propsVars.length === 0 ? (
+          <div className="text-center">
+            <p className="text-white">No props available</p>
           </div>
-        ))}
+        ) : (
+          componentConfig.propsVars.map((prop) => (
+            <div key={prop.name} className="mb-4 row">
+              <label className="col-sm-3 col-form-label text-white">
+                {prop.name}:
+              </label>
+              <div className="col-sm-9 mt-2">
+                <MonacoEditor
+                  value={formInputs[prop.name]?.value || ""}
+                  onChange={(value) => handleInputChange(prop.name, value)}
+                  height="50px"
+                  id={prop.id}
+                  width="80%"
+                  language="javascript"
+                />
+              </div>
+            </div>
+          ))
+        )}
         <div className="d-flex justify-content-end mt-4">
-          <Button className="btn btn-primary" onClick={updateProps}>
+          <Button
+            className="btn btn-primary"
+            onClick={updateProps}
+            disabled={componentConfig.propsVars.length === 0}
+          >
             Update
           </Button>
         </div>
