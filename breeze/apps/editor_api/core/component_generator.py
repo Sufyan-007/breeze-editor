8702e70f33f557acbff98f70b229d0ff05a501e3
sgazdhar <sgazdhar@argusoft.com>
2024-07-12 15:39:23 +0530

    Resolve merge changes

--- breeze/apps/editor_api/core/component_generator.py
+++ breeze/apps/editor_api/core/component_generator.py
@@ -101,7 +101,7 @@ class ComponentGenerator():
         # print("REACTCOMPONENT")
         # print(react_component_code)
 
-        # formatted_code = subprocess.check_output(['npx', 'prettier', '--parser', 'babel'], input=react_component_code, text=True)
+        formatted_code = subprocess.check_output(['npx', 'prettier', '--parser', 'babel'], input=react_component_code, text=True)
         formatted_code = react_component_code
         
         # Create parent dir if not exists
