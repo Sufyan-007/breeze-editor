af90dd38d091b4f3110b6959b8423b2db08fa4b9
VaranVilkhu <54389360+VaranVilkhu@users.noreply.github.com>
2024-07-19 13:16:21 +0530

    added delete functionality

--- breeze/apps/editor_api/core/resource_config_service.py
+++ breeze/apps/editor_api/core/resource_config_service.py
@@ -40,10 +40,7 @@ class ResourceConfigGenerator:
         create_parent_dir_if_not_exists(os.path.dirname(path))
         with open(path, 'w') as file:
             json.dump(data, file, indent=4)
-
-    def generate_unique_id(self):
-        return str(uuid.uuid4())
-    
+            
     def determine_lineage(self, file_path):
         directory_management = self.read_config_file(self.directory_management_path)
         
@@ -59,18 +56,11 @@ class ResourceConfigGenerator:
           
         return lineage
             
-    def update_directory_management(self, file_name, file_path, file_type):
-        print("Updating directory management...")
-        print(file_name, file_path, file_type, "parameters")
-
+    def update_directory_management(self, file_id, file_name, file_path, file_type):
         # Read existing directory management configuration
         directory_management = self.read_config_file(self.directory_management_path)
         if not directory_management:
-            print(f"No existing directory management configuration found at {self.directory_management_path}")
-
-        # Generate unique ID for the new resource
-        unique_id = self.generate_unique_id()
-        
+            print(f"No existing directory management configuration found at {self.directory_management_path}")        
         # Determine the lineage for the new resource based on the file path
         lineage = self.determine_lineage(file_path)
         if not lineage:
@@ -78,10 +68,10 @@ class ResourceConfigGenerator:
 
         # Create a new resource entry
         new_resource = {
-            unique_id: {
+            file_id: {
                 "name": file_name,
                 "lineage": lineage,
-                "id": unique_id,
+                "id": file_id,
                 "tag": file_type.upper(),
                 "type": "FILE"
             }
@@ -123,7 +113,7 @@ class ResourceConfigGenerator:
         full_path = os.path.join(self.app_config['APP_SOURCE_DIR'], file_path)
         self.save_file(file_id, file_name, full_path)
 
-        self.update_directory_management(file_name, file_path, file_type)
+        self.update_directory_management(file_id, file_name, file_path, file_type)
         return config_data
 
     def get_uploaded_files(self):
@@ -138,7 +128,6 @@ class ResourceConfigGenerator:
             del config_data[file_id]
             self.write_config_file(self.config_file_path, config_data)
 
-            # Remove leading 'src/' if present
             if file_path.startswith('/src/'):
                 file_path = file_path[len('/src/'):]
 
@@ -146,7 +135,24 @@ class ResourceConfigGenerator:
 
             if os.path.exists(full_path):
                 os.remove(full_path)
-    
+
+                # Update directory_management.json after deleting the file
+        directory_management = self.read_config_file(self.directory_management_path)
+        if file_id in directory_management:
+            del directory_management[file_id]
+
+            try:
+                self.write_config_file(self.directory_management_path, directory_management)
+                print(f"Successfully updated directory management configuration at {self.directory_management_path}")
+            except IOError as e:
+                print(f"Error writing to {self.directory_management_path}: {e}")
+            else:
+                print(f"Error: File {full_path} does not exist.")
+        else:
+            print(f"Error: File ID {file_id} not found in config data.")
+
+            
+            
     def file_duplicacy(self, file_name):
         existing_config = self.read_config_file(self.config_file_path)
         return any(value['name'] == file_name for value in existing_config.values())
--- breeze/apps/editor_api/views.py
+++ breeze/apps/editor_api/views.py
@@ -282,7 +282,6 @@ class ProjectConfig(APIView):
         data = json.loads(request.body.decode("utf-8"))
         data['defaultComponent'] = "Main"
         data["projectName"] = data["name"]
-        # data["selectedTemplate"]= data["selectedTemplate"]
         data["name"] = data['name'].lower().replace(" ", "_")
         generated_paths = os.path.join(
             os.path.dirname(os.getcwd()), "generated_projects")
