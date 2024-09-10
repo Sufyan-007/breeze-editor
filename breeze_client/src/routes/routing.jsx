75f5de748f44ec3a5db3f23e18434f7202b8f48d
hsutaria <hsutaria@argusoft.com>
2024-09-10 10:25:55 +0530

    redirect routing to home/login page

--- breeze_client/src/routes/routing.jsx
+++ breeze_client/src/routes/routing.jsx
@@ -1,21 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
-import Login from "../modules/authentication/pages/login/Login";
-import LandingPage from "../modules/landingPage/pages/LandingPage";
-import TestingSelectField from "../modules/justTestingNewComponents/TestingSelectField";
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import Login from '../modules/authentication/pages/login/Login';
+import LandingPage from '../modules/landingPage/pages/LandingPage';
+import ProjectPage from '../modules/project/pages/ProjectPage';
+
+const checkAccessToken = () => {
+  const token = localStorage.getItem('accessToken');
+  return token !== null;
+};
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: checkAccessToken() ? <Navigate to="/home" /> : <Navigate to="/login" />,
+  },
+  {
+    path: '/home',
     element: <LandingPage />,
   },
   {
-    path: "/TestingComps",
-    children: [
-      { path: "selectField", element: <TestingSelectField /> },
-    ]
+    path: '/login',
+    element: <Login />,
   },
   {
-    path: "/login",
-    element: <Login />,
+    path: '/project/:projectName',
+    element: <ProjectPage />,
   },
 ]);
