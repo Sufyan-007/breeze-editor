a6f288f314870e8bbc31af0379a3701edcce6917
hsutaria <hsutaria@argusoft.com>
2024-09-11 11:49:08 +0530

    top bar styling

--- breeze_client/src/modules/project/components/TopBar.jsx
+++ breeze_client/src/modules/project/components/TopBar.jsx
@@ -7,7 +7,7 @@ function TopBar() {
         <div className="blank"></div>
       </div>
       <div className="col-md-4 col-sm-6 d-flex justify-content-xl-center justify-content-start">
-        <ul className="nav nav-pills custom-nav rounded" id="pills-tab" role="tablist">
+        <ul className="nav nav-pills custom-nav rounded br-background-secondary" id="pills-tab" role="tablist">
           <li className="nav-item me-2" role="presentation">
             <button
               className="nav-link"
@@ -19,7 +19,7 @@ function TopBar() {
               aria-controls="pills-home"
               aria-selected="false"
             >
-              <i className="bi bi-code-slash"></i>
+              <i className="bi bi-code-slash me-1"></i>
               Code
             </button>
           </li>
@@ -34,17 +34,7 @@ function TopBar() {
               aria-controls="pills-profile"
               aria-selected="true"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="12"
-                height="12"
-                fill="currentColor"
-                className="bi bi-eye"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
-              </svg>
+              <i className="bi bi-eye me-1"></i>
               Preview
             </button>
           </li>
