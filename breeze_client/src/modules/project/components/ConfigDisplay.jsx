3f9686b7d39bc549aa9c2997f7ea292a0a9aecc8
hsutaria <hsutaria@argusoft.com>
2024-09-17 13:04:16 +0530

    Config screens as per type

--- breeze_client/src/modules/project/components/ConfigDisplay.jsx
+++ breeze_client/src/modules/project/components/ConfigDisplay.jsx
@@ -1,6 +1,23 @@
-// to add various config screens as per type. [Eg. Third party libs, routing, etc]
-function ConfigDisplay() {
-  return <div className="vh-100 br-background-primary br-text-primary">Config</div>;
+import PropTypes from 'prop-types';
+
+function ConfigDisplay({ configType }) {
+  const renderConfigScreen = () => {
+    switch (configType) {
+      case 'routing':
+        return <>Routing Config Screen</>;
+      case 'third-party':
+        return <>Third party Config Screen</>;
+      case 'services':
+        return <>Services Config Screen</>;
+      default:
+        return <div>Select a configuration type</div>;
+    }
+  };
+  return <div className="vh-100 br-background-primary br-text-primary">{renderConfigScreen()}</div>;
 }
+
+ConfigDisplay.propTypes = {
+  configType: PropTypes.string,
+};
 
 export default ConfigDisplay;
