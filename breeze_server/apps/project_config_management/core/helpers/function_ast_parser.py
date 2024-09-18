6e5e5f9156026d12aa6e3d30fa5e93c9390c0d35
Jap <jhirpara@argusoft.com>
2024-09-18 17:11:09 +0530

    ["added structure and instructions to create new APIs"]

---
+++ breeze_server/apps/project_config_management/core/helpers/function_ast_parser.py
@@ -0,0 +1,232 @@
+# from breeze.apps.editor_api.core.component_config_service import ComponentConfigService
+from .function_helper_consts import OPERATION_TYPES 
+RESOURCES={
+    "STATE/UUID1":{
+        "name":"xyz"
+    },
+    "SERVICE/UUID1":{
+        "functionName":"createUser"
+    },
+     "var1":{
+        "name":"val"
+    },
+     "var2":{
+        "name":"va1"
+    }
+}
+
+class FunctionParser:
+    def __init__(self, resources=None):
+        self.resources = resources 
+    
+    def generate_statement_code(self,config):
+        
+        if not config.get('type'):
+            return ""
+        
+        
+        elif config["type"] == "BLOCK":
+            statements = "\n".join([ self.generate_statement_code(code) for code in config['statements']])
+            return f"""{{ 
+                {statements}
+            }}"""
+        
+        elif config['type'].upper() == "FUNCTION":
+            func_name = ""
+            if not config.get('isAnonymous'):
+                func_name = f"const {config['name']} = "
+            
+            return f"""{func_name} {"" if config.get('isAsync') is not True else "async"} ( {", ".join([
+                    self.get_function_param(p) for p in config.get("parameters",[])
+                ])} ) => {self.generate_statement_code(config.get('bodyConfig',{}))}"""
+        
+        
+        elif config["type"] == "DECLARATION":
+            declaration_type = config.get("declarationType","const") 
+            varName = config["varName"]
+            if declaration_type == "const" or config.get("value",False):
+                value = self.get_value_code(config["value"])
+                return f"""{declaration_type} {varName} = {value}"""
+            else:
+                return f"""{declaration_type} {varName} """
+            
+        elif config["type"] == "ASSIGNMENT": 
+            varName = config["varName"]
+            value = self.get_value_code(config["value"])
+            return f"""{varName} = {value}"""
+        
+        
+        
+        elif config['type'] == "FUNCTION_CALL":
+            return self.get_function_call_code(config)
+        elif config['type']== "CHAINED_FUNCTIONS":
+            function_calls = [ self.get_function_call_code(func,True) for func in config["functions"] ]
+            isAwaited = ""
+            if config.get("isAwaited", False):
+                isAwaited = "await "
+            return isAwaited+".".join(function_calls)
+        
+        elif config['type'] == "CUSTOM":
+            return config.get("body","")
+        
+        elif config['type'] == "IF_BLOCK":
+            code = f""" if ({self.get_value_code(config["condition"])}) {self.generate_statement_code(config.get('bodyConfig',{}))} 
+            """
+            if config.get('elseIf', False):
+                for x in config.get('elseIf'):
+                    code += f"""else if({self.get_value_code(x["condition"])}) {self.generate_statement_code(x.get('bodyConfig',{}))}"""
+            if config.get('elseBody', False):
+                code += f"""else  {self.generate_statement_code(config.get('elseBody',{}))}
+            """
+            return code
+        
+        elif config['type'] == "FOR_BLOCK":
+            if config.get('loopType', "STANDARD") == "STANDARD":
+                initializer = self.generate_statement_code(config["initializer"])
+                configiton = self.generate_statement_code(config["initializer"])
+                
+            else:
+                iterator = f"""{config["iterator"].get("declarationType","const ")} {config["iterator"]["name"]}"""
+                iterate = "in" if config.get('loopType')=="FOR_IN" else "of"
+                iterable = self.get_value_code(config["iterable"])
+                return f""" for ( {iterator} {iterate} {iterable})
+                    {self.generate_statement_code(config.get('bodyConfig'))}
+                """
+            pass
+        
+        elif config['type'] == 'TRY_CATCH':
+            tryBody = f"""try {self.generate_statement_code(config["tryBody"])}"""
+            catchBody = f"""catch (err) {self.generate_statement_code(config["catchBody"])}"""
+            
+            finallyBody = ""
+            if config.get('finallyBody'):
+                finallyBody = f"""finally {self.generate_statement_code(config["finallyBody"])}"""
+            
+            return " ".join([tryBody,catchBody,finallyBody])
+        
+        elif config['type'] == "WHILE_BLOCK":
+            return f""" while ({self.get_value_code(config["condition"])}) {self.generate_statement_code(config.get('bodyConfig',{}))} 
+        """
+        
+        elif config['type'] == "DO_WHILE_BLOCK":
+            return f"""do  {self.generate_statement_code(config.get('bodyConfig',{}))} while ({self.get_value_code(config["condition"])}) 
+        """
+        
+        
+        elif config['type'] == "RETURN":
+            return f""" return {self.get_value_code(config.get("value",{}))}
+        """
+        
+        elif config["type"] == "OPERATION":
+            return f" {self.get_operation_code(config)} "
+        
+        return ""
+        
+    def get_condition(self,config):
+        return "true"
+    
+    
+    def get_function_param(self,param):
+        if param.get('type',"ANY") != "OBJECT" or not param.get('properties',False) or not param.get("destructured",False) :
+            return param["name"]
+        else:
+            return f"""{{ {", ".join([
+                self.get_function_param(p) for p in param["properties"]
+                ])} }}"""
+        
+    
+    def get_resource_by_id(self,ref):
+        for resource in self.resources:
+            if resource.get("id") == ref:
+                return resource
+        raise IndexError(f"Resource with id {ref} not found.")
+    
+    def get_value_code(self,value):
+        ref = value.get("$ref")
+        type = value.get("type","UNDEFINED")
+        if ref:
+            resource = self.get_resource_by_id(ref)
+            return resource["name"]
+        else:
+            if type == "STRING":
+                return f" '{value['value']}' "
+            
+            elif type == "NUMERIC" or type == "TOKEN":
+                return value["value"]
+            
+            elif type == "UNDEFINED":
+                return "undefined"
+            
+            elif type == "NULL":
+                return "null"
+            
+            elif type == "BOOLEAN":
+                if value["value"] and value["value"]!="false":
+                    return "true"
+                else:
+                    return "false"
+            
+            elif type == "OBJECT":
+                return f"""{{ {",".join([ f" {x} : {self.get_value_code(value['properties'][x])}" for x in value.get("properties") ])}}}"""
+            
+            elif type == "ARRAY":
+                return f"[{', '.join([self.get_value_code(x) for x in value.get('values', [])])}]"
+
+            elif type == "OPERATION":
+                return self.get_operation_code(value)
+            
+            elif type == "FUNCTION" or type == "CALLBACK":
+                return self.generate_statement_code(value)
+            
+            elif type == "CUSTOM":
+                return value["value"]
+
+            elif type == "FUNCTION_CALL":
+                return self.generate_statement_code(value)
+            
+            elif type == "CHAINED_FUNCTIONS":
+                return self.generate_statement_code(value)
+
+        return ""
+        
+    def get_function_call_code(self,config,disableAwait = False):
+        ref = config.get("$ref",None)
+        if ref:
+            functionName = RESOURCES[config["$ref"]]["functionName"]
+        else:
+            functionName = config['functionName']
+        isAwait = ""
+        if config.get("isAwaited") and not disableAwait:
+            isAwait = "await "
+        return f"""{isAwait}{functionName}({self.get_parameter_mapping(config)})
+        """
+        
+        
+    def get_parameter_mapping(self,config):
+        param_list =[]
+        for param in config.get("parameters",[]):
+            param_list.append(self.get_value_code(param))
+        return ", ".join([str(x) for x in param_list])
+    
+    def get_operation_code(self,config):
+        if config["operationType"] == "UNARY":
+            return f" {config['operation']}{self.get_operand_code(config['operand'] )}"
+
+        elif config["operationType"] == "BINARY":
+            operand1 = self.get_operand_code(config['operand1'] )
+            operand2 = self.get_operand_code(config['operand2'] )
+            return f" {operand1}{config['operation']}{operand2} "
+            
+        
+        elif config["operationType"] == "TERNARY":
+            operand1 = self.get_operand_code(config['operand1'] )
+            operand2 = self.get_operand_code(config['operand2'] )
+            operand3 = self.get_operand_code(config['operand3'] )
+            return f" {operand1} ? {operand2} : {operand3} "
+            
+    def get_operand_code(self, operand):
+        if operand.get("type") == "OPERATION":
+            return f" ( {self.get_value_code(operand )} ) "
+        else:
+            return self.get_value_code(operand)
+        
--- breeze_server/apps/project_config_management/urls.py
+++ breeze_server/apps/project_config_management/urls.py
@@ -1,3 +1,58 @@
 from django.urls import path
 
-urlpatterns = []
+urlpatterns = [
+    
+    # TODO: create files in views and add required method
+    # past endpoint: POST - /editor/write-config 
+    # both POST, PUT and DELETE will be handled for all 
+    
+    # component -> functions, variables, other props
+    # services
+    # redux
+    # reducers
+    # context
+    # 
+    path('write-resource-config', ),
+    
+    # to manage whole route scenario individually
+    path('manage-routes', ),
+    
+    # to manage whole API client scenario individually
+    path('manage-api-client', ),
+    # ......./api-client-generator/fetch-all-intermediates
+    # ......./api-client-generator/edit-module-name/
+    # ......./api-client-generator/convert-standard-json
+    # ......./api-client-generator/add-schema/
+    # ......./api-client-generator/delete-schema/
+    # ......./api-client-generator/edit-schema/
+    # one API is present in code generator module
+    
+    
+    # APIs with derived data
+    path('get-file-path', ),
+]
+
+# new endpoint -> /editor/get-file-path
+# in write-resource-config -> 
+    # /editor/update-html-config
+    # /editor/write-services/
+# manage-routes -> /editor/handle-base-route
+
+
+# resource_obj = {
+#   name: 'resource1', ( like component_config, app_basic_config, etc)
+#   category: __through_some_enum__,
+#   select : [abc, pqr, ...] -> the properties i want to see
+#   filter : { 
+#              operation : "ANY | All"
+#              conditions :[
+#                  id : [{ abc: 12 }]
+#                  Name : [ 'jap', 'patel' ]
+#                  AGE : [{ GT: 7}]
+#              ]
+#          }
+#   order : "ASCENDING"
+#   limit : 5
+#   offset : 2
+#   count : 15
+# }
