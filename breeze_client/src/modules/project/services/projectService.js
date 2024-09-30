f05014efaee1a02451070ea2698972f0010b6c2c
AnshikX <athakur@argusoft.com>
2024-09-30 17:43:36 +0530

    create project api itnegeration

--- breeze_client/src/modules/project/services/projectService.js
+++ breeze_client/src/modules/project/services/projectService.js
@@ -1,13 +1,10 @@
+import { callApiClient } from '../../../utils/breezeApiCall';
+
 export const getAllProjects = async () => {
+  const url = `${import.meta.env.VITE_BREEZE_BACKEND_HOST}/api/project/get-all/`;
   try {
-    const response = await fetch(`${import.meta.env.VITE_BREEZE_BACKEND_HOST}/api/project/get-all/`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const projects = await response.json();
-    return projects;
+    const response = await callApiClient(url, 'GET');
+    return response;
   } catch (error) {
     console.error('Error fetching projects:', error.message);
     throw error;
@@ -15,20 +12,24 @@ export const getAllProjects = async () => {
 };
 
 export const getFolderConfig = async (id = null, projectName) => {
+  const url = `${import.meta.env.VITE_BREEZE_BACKEND_HOST}/api/directory/${projectName}/get/?target_id=${id}`;
   try {
-    const response = await fetch(
-      `${import.meta.env.VITE_BREEZE_BACKEND_HOST}/api/directory/${projectName}/get/?target_id=${id}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    const projectConfig = await response.json();
-    return projectConfig;
+    const response = await callApiClient(url, 'GET');
+    return response;
   } catch (error) {
     console.error('Error fetching project config:', error.message);
+    throw error;
+  }
+};
+
+export const createProject = async (projectData) => {
+  const url = `${import.meta.env.VITE_BREEZE_BACKEND_HOST}/api/project/add/`;
+  const payload = projectData;
+  try {
+    const response = await callApiClient(url, 'POST', payload);
+    return response;
+  } catch (error) {
+    console.error('Error creating project:', error.message);
     throw error;
   }
 };
