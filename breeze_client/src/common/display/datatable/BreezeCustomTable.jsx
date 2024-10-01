6145bfa81373aa8570f54605ec70f478171c801c
VaranVilkhu <54389360+VaranVilkhu@users.noreply.github.com>
2024-10-01 16:43:53 +0530

    changes to jsx

--- breeze_client/src/common/display/datatable/BreezeCustomTable.jsx
+++ breeze_client/src/common/display/datatable/BreezeCustomTable.jsx
@@ -143,7 +143,7 @@ const CustomTable = ({
           Previous
         </button>
         <span>
-          Page {currentPage} of {Math.ceil((data?.length || 0) / pageSize)}
+          Page {currentPage} of {Math.max(1, Math.ceil((data?.length || 0) / pageSize))}
         </span>
         <button
           className="btn br-text-primary med-font"
