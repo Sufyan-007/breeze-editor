ee267b93d8e82c6e995c56e6fc1775309bb6d537
Subham Kumar <=subhamk@argusoft.com>
2024-10-03 14:12:53 +0530

    add swagger doc for api

---
+++ breeze_server/apps/project_management/swagger_schema/env_apis_schema.py
@@ -0,0 +1,43 @@
+from drf_yasg import openapi
+
+
+set_env_schema = {
+    'rb':openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'environmentName':openapi.Schema(type=openapi.TYPE_STRING,description='name of environment')
+        }
+    ),
+     'response_200':openapi.Response(
+            description='success',
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'status':openapi.Schema(type=openapi.TYPE_STRING),
+                    'message':openapi.Schema(type=openapi.TYPE_STRING)
+                },
+                examples={'application/json':{'message':'Environment default has been set as active'}}
+            )
+    ),
+     'response_500':openapi.Response(
+            description='Request is failed due to an error.',
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'error':openapi.Schema(type=openapi.TYPE_STRING)
+                }
+            )
+    ),
+}
+
+get_env_config_schema = {
+    'response_500':openapi.Response(
+            description='Request is failed due to an error.',
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'error':openapi.Schema(type=openapi.TYPE_STRING)
+                }
+            )
+    ),
+}
--- breeze_server/apps/project_management/views/env_apis.py
+++ breeze_server/apps/project_management/views/env_apis.py
@@ -5,8 +5,18 @@ from apps.common.utils.file_helpers.json_handler import read_project_config_file
 from apps.common.constants.consts import CONFIG_FILES_PATH, CONFIG_PATH
 from ..core.environment_management import set_environment
 from django.http import JsonResponse
+from drf_yasg.utils import swagger_auto_schema
+from ..swagger_schema.env_apis_schema import set_env_schema,get_env_config_schema
 
-
+@swagger_auto_schema(
+    method='post',
+    request_body=set_env_schema['rb'],
+    responses={
+        200:set_env_schema['response_200'],
+        500:set_env_schema['response_500']
+    },
+    tags=['environment']
+)
 @csrf_exempt
 @api_view(['POST'])
 def set_env(request, project_id):
@@ -20,7 +30,13 @@ def set_env(request, project_id):
     except Exception as e:
         print(f"Error: {e}")
         return JsonResponse({'error': 'Server error'}, status=500)
-    
+
+@swagger_auto_schema(
+    method='get',
+    request_body=None,
+    responses=None,
+    tags=['environment']
+)
 @csrf_exempt
 @api_view(['GET'])
 def get_env_config(request, project_id):
@@ -29,7 +45,13 @@ def get_env_config(request, project_id):
     except Exception as e:
         print(f"Error: {e}")
         return JsonResponse({'error': 'Server error'}, status=500)
-    
+
+@swagger_auto_schema(
+    method='post',
+    request_body=None,
+    responses=None,
+    tags=['environment']
+)
 @csrf_exempt
 @api_view(['POST'])
 def add_env_config(request, project_id):
@@ -39,7 +61,12 @@ def add_env_config(request, project_id):
         print(f"Error: {e}")
         return JsonResponse({'error': 'Server error'}, status=500)
     
-    
+@swagger_auto_schema(
+    method='put',
+    request_body=None,
+    responses=None,
+    tags=['environment']
+)
 @csrf_exempt
 @api_view(['PUT'])
 def update_env_config(request, project_id):
@@ -48,7 +75,13 @@ def update_env_config(request, project_id):
     except Exception as e:
         print(f"Error: {e}")
         return JsonResponse({'error': 'Server error'}, status=500)
-    
+
+@swagger_auto_schema(
+    method='delete',
+    request_body=None,
+    responses=None,
+    tags=['environment']
+)
 @csrf_exempt
 @api_view(['DELETE'])
 def delete_env_config(request, project_id):
