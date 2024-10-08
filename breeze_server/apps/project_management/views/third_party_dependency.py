1fb0962a4d973fdb75475228cd6658e3b1980b2e
hsutaria <hsutaria@argusoft.com>
2024-10-08 16:04:30 +0530

    apis for third party dependencies

--- breeze_server/apps/project_management/views/third_party_dependency.py
+++ breeze_server/apps/project_management/views/third_party_dependency.py
@@ -1,43 +1,78 @@
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
-from apps.common.constants.consts import CONFIG_FILES_PATH, CONFIG_PATH
-from apps.common.utils.file_helpers.json_handler import read_project_config_file
+from apps.common.utils.file_helpers.config_handler import get_breeze_config_file
 from django.http import JsonResponse
+from ..core.third_party_dependency_service import (
+    add_package_to_dependencies,
+    update_package_in_dependencies,
+    delete_package_from_dependencies
+)
 import json
+
+
+def parse_request_data(request):
+    try:
+        data = json.loads(request.body.decode("utf-8"))
+        package_name = data.get("name")
+        package_version = data.get("version")
+
+        if not package_name or (request.method != "DELETE" and not package_version):
+            raise ValueError("Both package name and version are required.")
+        return package_name, package_version
+    except json.JSONDecodeError:
+        raise ValueError("Invalid JSON data.")
 
 
 @csrf_exempt
 @api_view(["POST"])
-def addThirdPartyDependency(request, projectName):
+def add_third_party_dependency(request, projectName):
     try:
-        data = json.loads(request.body)
-        print(data)
-        return JsonResponse("Done", status=200)
+        package_name, package_version = parse_request_data(request)
+        add_package_to_dependencies(projectName, package_name, package_version)
+        return JsonResponse({"message": "Package added successfully"}, status=200)
+    except ValueError as ve:
+        return JsonResponse({"error": str(ve)}, status=400)
     except Exception as e:
-        return JsonResponse({"message": str(e)}, status=500)
+        return JsonResponse({"error": str(e)}, status=500)
 
 
 @csrf_exempt
 @api_view(["GET"])
-def getThirdPartyDependency(request, projectName):
+def get_third_party_dependency(request, projectName):
     try:
-        app_config_dir = f"{CONFIG_PATH}/{projectName}"
-        app_config = read_project_config_file(
-            app_config_dir, CONFIG_FILES_PATH["APP_CONFIG"]
-        )
-        dependencies = app_config["dependencies"]
+        app_config = get_breeze_config_file(projectName, "APP_CONFIG")
+        dependencies = app_config.get("dependencies", {})
         return JsonResponse(dependencies, status=200)
     except Exception as e:
-        return JsonResponse({"message": str(e)}, status=500)
+        return JsonResponse({"error": str(e)}, status=500)
 
 
 @csrf_exempt
 @api_view(["PUT"])
-def updateThirdPartyDependency(request, projectName):
-    return ""
+def update_third_party_dependency(request, projectName):
+    try:
+        package_name, package_version = parse_request_data(request)
+        update_package_in_dependencies(projectName, package_name, package_version)
+        return JsonResponse({"message": "Package updated successfully"}, status=200)
+    except ValueError as ve:
+        return JsonResponse({"error": str(ve)}, status=400)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
 
 
 @csrf_exempt
 @api_view(["DELETE"])
-def deleteThirdPartyDependency(request, projectName):
-    return ""
+def delete_third_party_dependency(request, projectName):
+    try:
+        data = json.loads(request.body.decode("utf-8"))
+        package_name = data.get("name")
+        
+        if not package_name:
+            return JsonResponse({"error": "Package name is required"}, status=400)
+        
+        delete_package_from_dependencies(projectName, package_name)
+        return JsonResponse({"message": "Package deleted successfully"}, status=200)
+    except ValueError as ve:
+        return JsonResponse({"error": str(ve)}, status=400)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
