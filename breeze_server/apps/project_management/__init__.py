3393647f3c6f8c7f1da8f82c849ded40da0af272
sgazdhar <sgazdhar@argusoft.com>
2024-10-08 14:32:49 +0530

    Bug fixes and added api to get port

--- breeze_server/apps/project_management/__init__.py
+++ breeze_server/apps/project_management/__init__.py
@@ -1,6 +1,8 @@
-from .utils.get_all_projects import get_all_projects
-from .communication.app_startup_manager import start_app
+import os
 
-projects = get_all_projects()
-for project in projects.values():
-    start_app(project)
+if os.environ.get('RUN_MAIN' ):
+    from .utils.get_all_projects import get_all_projects
+    from .communication.app_startup_manager import start_app
+    projects = get_all_projects()
+    for project in projects.values():
+        start_app(project)
