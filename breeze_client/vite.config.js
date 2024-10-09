688c332b6ec322eb6dd85dd41da181508d359494
Jap <jhirpara@argusoft.com>
2024-10-09 17:45:03 +0530

    ["added env changes"]

--- breeze_client/vite.config.js
+++ breeze_client/vite.config.js
@@ -5,9 +5,15 @@ import dotenv from 'dotenv';
 export default defineConfig(({ mode }) => {
   // Load the environment variables from the appropriate .env file
   const env = dotenv.config({ path: `.env.${mode}` }).parsed;
+  const host = env.VITE_HOST || '0.0.0.0';
+  const port = env.VITE_PORT || 5173;
   return {
     define: {
       'process.env': env,
+    },
+    server: {
+      host: host,
+      port: port,
     },
     plugins: [react()],
   };
