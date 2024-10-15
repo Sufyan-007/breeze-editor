17396930f20ae1b0546501a026622d69bb0a81aa
Jap <jhirpara@argusoft.com>
2024-10-15 12:13:42 +0530

    ["handled package.json dependency generation, get-port fix, "]

--- breeze_server/apps/project_management/views/handle_general_proj_apis.py
+++ breeze_server/apps/project_management/views/handle_general_proj_apis.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from ..utils.validate_add_proj_data import load_proj_data
 from ..utils.get_all_projects import get_all_projects
+from ..utils.start import start_project
 from apps.common.constants.consts import CONFIG_FILES_PATH, CONFIG_PATH
 from apps.common.utils.file_helpers.json_handler import read_project_config_file
 from apps.project_config_management.core.config_files_handlers import add_dirs_configs
@@ -72,6 +73,8 @@ def add(request):
         # if logo_file:
         #     resource_config_generator = ResourceConfigGenerator(data["name"])
         #     resource_config_generator.update_config(logo_file.name, '/src/assets', "", logo_file_id)
+        
+        start_project(data["name"])
         response = {"name": data["name"]}
         return JsonResponse(response, status=200)
     except Exception as e:
