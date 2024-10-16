7a46f7fea4c3c6e2a06c1f4da55770b22608478e
AnshikX <athakur@argusoft.com>
2024-10-16 12:46:05 +0530

    removed console

--- breeze_client/src/utils/breezeApiCall.jsx
+++ breeze_client/src/utils/breezeApiCall.jsx
@@ -49,7 +49,6 @@ export async function callApiClient(
     };
 
     const response = await fetch(url, requestOptions);
-    console.log(response);
     if (!response.ok) {
       throw new Error(`API call failed with status ${response.status}`);
     }
