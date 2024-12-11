1fb1230d0daa71adab86322c9ad128237d92bf14
apainuli <apainuli@argusoft.com>
2024-12-11 17:14:25 +0530

    added type function handling in schema

--- breeze_server/apps/code_generator/core/api_client_generator.py
+++ breeze_server/apps/code_generator/core/api_client_generator.py
@@ -79,6 +79,7 @@ def addEntities(functionDetails, fileId, projectId):
         functionDetails = [functionDetails]
     
     for function in functionDetails:
+        # function_schema = generate_function_schema(function.get("schema"))
         entityManager.add_or_update_entity(
             entityId=function["id"],
             fileId=fileId,
@@ -87,6 +88,27 @@ def addEntities(functionDetails, fileId, projectId):
             defaultExport=False,
             schema=function.get("schema",{"type" : "ANY"})
         )
+        
+        
+def generate_function_schema(parameters_config):
+    function_template = {
+        "type": "FUNCTION",
+        "parameters": [],
+        "returnType":{}
+    }
+    parameters = []
+    for parameter in parameters_config:
+        if parameter.get("properties"):
+            for param in parameter:
+                new_param = {
+                    "name": param.get("name"),
+                    "destructured": False,
+                    "schema": {},
+                    "defaultValue": {}
+                }
+                
+
+
 def create_websocket_hook_file( filename, app_config):
     # preprare new service file for each tag
     folder_name = "hooks"
@@ -167,7 +189,7 @@ def generate_service_function( model, anonymous, app_name,service_type, service_
     
     # set request headers
     combined_headers = set_request_headers(model,app_name)
-    param_headers = combined_headers["param_headers"] if combined_headers else []
+    param_headers = combined_headers["param_headers"] if combined_headers else {}
     if combined_headers.get("header_argument"):
         function_args.append(combined_headers.get("header_argument"))
     
@@ -176,21 +198,27 @@ def generate_service_function( model, anonymous, app_name,service_type, service_
     # set request body if given
     body_items = set_request_body(model,app_name,module_id)
     #needs to be changed when body will be a dictionary instead of list
-    body_params = {"type": "OBJECT", "name": "BodyDetails", "properties": {}}
+    # body_params = {"type": "OBJECT", "name": "BodyDetails", "properties": {}}
+    body_params = {"name":"BodyDetails", "destructured": False, "schema": {"selection": "anyOf", "types":[]}}
     if body_items:
         if body_items.get("RAW"):
-            body_params = body_items["RAW"]["body_params"] 
+            body_params["schema"]["types"].append(body_items["RAW"]["body_params"])
         elif body_items.get("BINARY"):
-            body_params = body_items["BINARY"]["body_params"] 
+            body_params["schema"]["types"].append(body_items["BINARY"]["body_params"])
+        else:
+            body_params["schema"]["types"].append({"type": "OBJECT", "properties": {}})
         if body_params:
             body_params["name"] = "BodyDetails";
     #writing all the required parameters into the config
     ######################################################################################################
     model_parameters = []
-    model_parameters.append({"type": "OBJECT", "name": "PathParameters", "properties": path_params})
-    model_parameters.append({"type": "OBJECT", "name": "QueryParameters", "properties": query_params})
+    model_parameters.append({"name": "PathParameters","destructured": False, "schema":{"selection":"anyOf","types": [{"type": "OBJECT",  "properties": path_params}]}})
+    model_parameters.append({"name": "QueryParameters","destructured": False, "schema":{"selection":"anyOf","types": [{"type": "OBJECT",  "properties": query_params}]}})
+    # model_parameters.append({"type": "OBJECT", "name": "QueryParameters", "properties": query_params})
     model_parameters.append(body_params)
-    model_parameters.append({"type": "OBJECT", "name": "HeaderDetails", "properties": param_headers})
+    model_parameters.append({"name": "HeaderDetails","destructured": False, "schema":{"selection":"anyOf","types": [{"type": "OBJECT",  "properties": param_headers}]}})
+
+    # model_parameters.append({"type": "OBJECT", "name": "HeaderDetails", "properties": param_headers})
     new_model = model.as_dict()
     new_model["parameters"] = model_parameters
     new_model["operation_id"] = convert_to_valid_variable_name(func_name)
@@ -563,12 +591,13 @@ def set_response_interceptor( auth, app_name):
     return interceptor_code
 
 def set_request_headers( model, app_name):
-    headers = {"param_headers": [], "body_headers": {}, "header_argument": ''}
+    headers = {"param_headers": {}, "body_headers": {}, "header_argument": ''}
     if model.request.headers:
         headers["header_argument"] = "HeaderDetails"
         for header in model.request.headers:
             if header.type == "USER_INPUT":
-                headers["param_headers"].append({"name": header.key, "type": "STRING"})
+                headers["param_headers"][header.key] = {"selection": 'anyOf', "types":[{"type": "STRING"}]}
+                # headers["param_headers"].append({"name": header.key, "type": "STRING"})
                 headers["body_headers"][header.key] = {"type": header.type, "value":f"HeaderDetails.{header.key}"}
             elif header.type == "STATIC":
                 headers["body_headers"][header.key] = {"type": header.type, "value":header.value}
@@ -744,12 +773,13 @@ def set_request_url(model,app_name):
         else:
             url = "${process.env.%s}" % config.get("envVars").get(url_env) + path
             # url = "${process.env.%s}" % config.get("envVars").get(url_env) + '/' + path
-    new_query_params =[]
-    new_path_params = []
+    new_query_params ={}
+    new_path_params = {}
     for params in model.request.parameters:
         if params.param_in == ParamsInEnum.QUERY:
             if params.param_type == "USER_INPUT":
-                new_query_params.append({"name":params.name, "type":params.type})
+                new_query_params[params.name] = {"selection": 'anyOf', "types":[{"type": params.type}]}
+                # new_query_params.append({"name":params.name, "types" : [{"type":params.type}]})
                 query_params.append("%s=${QueryParameters.%s}" % (params.name, params.name))
                 if 'QueryParameters' not in function_args:
                     function_args.append('QueryParameters')
@@ -765,7 +795,8 @@ def set_request_url(model,app_name):
             
         elif params.param_in == ParamsInEnum.PATH:
             if params.param_type == "USER_INPUT":
-                new_path_params.append({"name":params.name, "type":params.type})
+                new_path_params[params.name] = {"selection": "anyOf", "types":[{"type": params.type}]}
+                # new_path_params.append({"name":params.name, "types" : [{"type":params.type}]})
                 url=url.replace(f"{{{params.name}}}", f"${{PathParameters.{params.name}}}")
                 if 'PathParameters' not in function_args:
                     function_args.append('PathParameters')
